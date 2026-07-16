14ebab86f513d11b59359389f168472945df7344
jaumpoul198 <jpo201086@gmail.com>
2026-07-16 07:10:23 -0300

    feat: add enterprise autonomy monitor

---
+++ src/pocketbot/enterprise/autonomy/__init__.py
@@ -0,0 +1,11 @@
+"""Enterprise Autonomy package."""
+
+from .autonomy_monitor import (
+    AutonomyMonitor,
+    AutonomySnapshot,
+)
+
+__all__ = [
+    "AutonomyMonitor",
+    "AutonomySnapshot",
+]
